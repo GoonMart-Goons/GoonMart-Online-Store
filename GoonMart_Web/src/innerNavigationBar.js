82665a517bd829e6cd13cde57de4ce81abea3118
Michelle Nkomo <2436785@students.wits.ac.za>
2023-04-16 10:54:12 +0200

    added nav bar to inner homepage (basic)

--- GoonMart_Web/src/innerNavigationBar.js
+++ GoonMart_Web/src/innerNavigationBar.js
@@ -9,6 +9,25 @@ function InnerNavigationBar() {
         <img src= "/goonmart-logo.png"/>
       </div>
       <div className="navbar-center">
+        <ul className="navbar-menu">
+          <li className="navbar-menu-item">
+            <a href="/InnerHomepage">Home</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/InnerHomepage">Orders</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/InnerHomepage">My Account</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/InnerHomepage">About</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/InnerHomepage">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
         <form className="search-form">
           <input type="text" className="search-input" placeholder="Search" />
           <button type="submit" className="search-button">
