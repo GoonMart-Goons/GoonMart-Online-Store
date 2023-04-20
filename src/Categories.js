83a772d3044c380346aa28565264e0cbc2b19d26
Milisa Sithole <2446396@students.wits.ac.za>
2023-04-21 00:08:31 +0200

    Categories now working with products in DB

--- src/Categories.js
+++ src/Categories.js
@@ -5,6 +5,11 @@ import { FaTshirt} from 'react-icons/fa';
 import { FaCoffee} from 'react-icons/fa';
 import { FaGamepad } from 'react-icons/fa';
 
+//FireBase imports
+import { db } from './config/Config'
+import { collection, query, where, getDocs } from 'firebase/firestore';
+
+//Catagories available
 const categories = [
   { id: 0, name: 'All', icon: <FaGlobe/> },
   { id: 1, name: 'Electronics', icon: <FaLaptop/> },
@@ -13,12 +18,25 @@ const categories = [
   { id: 4, name: 'Toys & Games', icon: <FaGamepad/> },
 ];
 
+//Query products that == selected category
+async function filterProdsByCategory(category){
+  const prodsRef = collection(db, 'Products')
+  const q = query(prodsRef, where('category', '==', category))
+  const qSnapshot = await getDocs(q)
+  //Queried documents
+  qSnapshot.forEach((doc) => {
+    console.log(doc.data())
+  })
+}
+
 function CategoryList() {
   const [activeCategoryId, setActiveCategoryId] = useState(0);
 
   const handleClick = (categoryId) => {
     setActiveCategoryId(categoryId);
     // Call a function to filter products by category
+    // console.log(categories[categoryId].name)
+    filterProdsByCategory(categories[categoryId].name)
   };
 
   return (
