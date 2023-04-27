60d0581e2ffcda162ddd200b2712460e26bbb7a3
Milisa Sithole <2446396@students.wits.ac.za>
2023-04-27 06:07:27 +0200

    search

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FaSearch} from 'react-icons/fa';
-  
+
+//FireBase Imports
+import { db } from './config/Config';
+import { collection, where, query, getDocs } from 'firebase/firestore'  
 
 function NavigationBar() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [results, setResults] = useState([])
+  //Somehow need to get this data to ProductGrid.js
+
+  const handleSearch = async(e) => {
+    e.preventDefault()
+
+    filterBySearch()
+    console.log(results)
+  }
+  
+
+  async function filterBySearch(){
+    const prodsRef = collection(db, 'Products')
+    const q = query(
+      prodsRef,
+      where('prodName', '>=', searchTerm),
+      where('prodName', '<=', searchTerm + '\uf8ff'),
+    )
+    const querySnapshot = await getDocs(q)
+    const DBproducts = querySnapshot.docs.map(doc => doc.data());
+    return DBproducts
+  }
+
+  React.useEffect(() => {
+    filterBySearch().then(products => {
+      setResults(products);
+    });
+  });
 
   return (
-    <nav className="navbar">
+    <nav className="navbar" onSubmit={handleSearch}>
       <div className="navbar-left">
         <img src= "/goonmart-logo.png" alt='goonmart logo'/>
         <span><b>GoonMart</b></span>
       </div>
       <div className="navbar-center">
         <form className="search-form">
-          <input type="text" className="search-input" placeholder="Search" />
+          <input type="text" className="search-input" placeholder="Search" 
+           onChange = {(e) => setSearchTerm(e.target.value)}/>
           <button type="submit" className="search-button">
             <FaSearch/>
           </button>
