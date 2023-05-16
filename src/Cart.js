aa4536783cf61ee2be1445b7a6cfe7adde2eba15
kudzaiSaurombe <82084840+kudzaiSaurombe@users.noreply.github.com>
2023-05-16 23:04:44 +0200

    update cart.js
    checkout button disabled when cart is empty, cart is empty message is shown

--- src/Cart.js
+++ src/Cart.js
@@ -3,51 +3,68 @@ import { useNavigate } from 'react-router-dom';
 import './Cart.css';
 import { CartContext } from './CartContext';
 
-
 const Cart = () => {
-  const { cartItems, removeItem, incrementItem, decrementItem } = useContext(CartContext);
-  const navigate = useNavigate();
+    const { cartItems, removeItem, incrementItem, decrementItem } = useContext(CartContext);
+    const navigate = useNavigate();
 
+    const navigateToProductPage = (id) => {
+        navigate(`/product/${id}`);
+    };
 
-  const navigateToProductPage = (id) => {
-    navigate(`/product/${id}`);
-  }
+    const handleProceedToCheckout = () => {
+        if (cartItems.length > 0) {
+            // Proceed to checkout logic here
+            navigate('/checkout');
+        }
+    };
 
-  return (
-      <div className="cart-container">
-        <h1>Cart</h1>
-        {cartItems.map(item => (
-          <div className = "cart-item">
-            <div className="product" key={item.id}>
-              <img src={item.image} alt={item.name} />
-              <div className="item-info">
-                <p>{item.name}</p>
-              </div>
-              </div>
-
-              <div className="quantity-control">
-                  <p>R {item.price * item.quantity}</p>
-                  <button onClick={() => decrementItem(item.id)}>-</button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => incrementItem(item.id)}>+</button>
-              </div>
-              
-              <div className='extras'>
-                <button onClick={() => navigateToProductPage(item.id)} className='view-cart'>View Product</button>
-                <button onClick={() => removeItem(item.id)} className='rmv-btn'>Remove</button>
-              </div>
-              
-            </div>
-        ))}
-        <div className='total'>
-          <h2>Total: R {cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0)}</h2>
+    return (
+        <div className="cart-container">
+            <h1>Cart</h1>
+            {cartItems.length === 0 ? (
+                <p>Your cart is empty.</p>
+            ) : (
+                <>
+                    {cartItems.map((item) => (
+                        <div className="cart-item" key={item.id}>
+                            <div className="product">
+                                <img src={item.image} alt={item.name} />
+                                <div className="item-info">
+                                    <p>{item.name}</p>
+                                </div>
+                            </div>
+                            <div className="quantity-control">
+                                <p>R {item.price * item.quantity}</p>
+                                <button onClick={() => decrementItem(item.id)}>-</button>
+                                <span>{item.quantity}</span>
+                                <button onClick={() => incrementItem(item.id)}>+</button>
+                            </div>
+                            <div className="extras">
+                                <button onClick={() => navigateToProductPage(item.id)} className="view-cart">
+                                    View Product
+                                </button>
+                                <button onClick={() => removeItem(item.id)} className="rmv-btn">
+                                    Remove
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                    <div className="total">
+                        <h2>Total: R {cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0)}</h2>
+                    </div>
+                    <div>
+                        <button
+                            className="checkout-btn"
+                            disabled={cartItems.length === 0}
+                            onClick={handleProceedToCheckout}
+                        >
+                            Proceed to Checkout
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
-
-        <div>
-          <button className='checkout-btn'>Proceed to Checkout</button>
-        </div>
-      </div>
-  );
-}
+    );
+};
 
 export default Cart;
