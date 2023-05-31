2186f55fe2585ab9c01cd71aa284ba97e3d900bb
Milisa Sithole <2446396@students.wits.ac.za>
2023-05-31 10:10:39 +0200

    Updated orders code

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,55 @@
-import React from 'react';
-  
+import React, {useState} from 'react';
+import './Reviews.css';
+import OrdersNavBar from './OrdersNavBar';
+import AddReview from './AddReview';
+
+//Firebase imports
+import { auth, db } from './config/Config'
+import { collection, addDoc, doc, getDoc, getDocs,  query, where } from 'firebase/firestore'
+
+//Gets userID of logged in user from Login page
+import { loggedInUserID } from './Login';
 
 export default function Orders() {
+  // *****************Reviews stuff -- Maybe should be in a diff file, but oh well**********************
+  // Dummy data
+  const [dummyOrders, setDummyOrders] = useState([
+    { id: '1', name: 'Product One', price: 10.99, quantity: 2, image: '/path/to/image1.jpg' },
+    { id: '2', name: 'Product Two', price: 20.99, quantity: 1, image: '/path/to/image2.jpg' },
+    { id: '3', name: 'Product Three', price: 30.99, quantity: 3, image: '/path/to/image3.jpg' },
+  ]);
+
+  //***************************Test variables*************************************
+  const userID = "dhAjexEe1kpENWuEUxbH"; //Test Case
+  const prodID = "WLBntFH5EyKNCXezD4SV"; // SMEG kettle
+  //const prodID = "9H6OJMKeExtZQE25v50m"; // iPhone
+  //const userID = "N8UIq9zUVYpIqY9TaqHD" // Kaji Katame
+
   return (
-      <div>
-        <h1> Your GoonMart Orders Will Appear Here!</h1>
-      </div>
+      <>  <OrdersNavBar/>
+
+        <div className="review-section">
+          <h1>{dummyOrders.length > 0 ? "Your Previous Orders" : "Your GoonMart Orders Will Appear Here!!!"}</h1>
+
+          <div className="orders-grid">
+            {/* ORDER ITEMS HERE */}
+            {dummyOrders.map((order) => (
+                <div key={order.id} className="order-item">
+                  <img src={order.image} alt={order.name} />
+                  <div className="order-item-info">
+                    <h2>{order.name}</h2>
+                    <p>Quantity: {order.quantity}</p>
+                    <p>Price: R{order.price}</p>
+
+                    <AddReview
+                      prodID = {prodID}
+                      userID = {userID /*loggedInUserID*/} />
+                  </div>
+                </div>
+            ))}
+          </div>
+
+        </div>
+    </>
   );
 }
